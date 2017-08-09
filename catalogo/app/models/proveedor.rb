c13dd6eb1120e41b446e9928ee80137374a3048d
Adrian <adh761@gmail.com>
2017-08-09 19:05:03 -0400

    validacion del lado del servidor

--- catalogo/app/models/proveedor.rb
+++ catalogo/app/models/proveedor.rb
@@ -1,3 +1,59 @@
 class Proveedor < ActiveRecord::Base
 	has_many :productos_detalles
+
+	validates :descripcion,
+		:presence => {
+			message: 'Este campo es requerido'},
+		:length => {
+			:maximum => 20,
+			:too_long => 'debe tener como maximo %{count} caracteres'}
+
+	validates :direccion,
+		:presence => {
+			message: 'Este campo es requerido'},
+		:length => {
+			:maximum => 50,
+			:too_long => 'Debe tener como maximo %{count} caracteres'}
+
+	validates :sucursal,
+		:presence => {
+			message: 'Este campo es requerido'},
+    :length => {
+			:maximum => 20,
+			:too_long => 'debe tener como maximo %{count} caracteres'}
+
+
+	validates :vendedor,
+		:presence => {
+			message: 'Este campo es requerido'},
+		:length => {
+			:maximum => 50,
+			:too_long => 'Debe tener como maximo %{count} caracteres'}
+
+
+	validates :telefono,
+		:presence => {
+			message: 'Este campo es requerido'},
+		:length => {
+			:maximum => 20,
+			:too_long => 'debe tener como maximo %{count} caracteres'},
+		:format => {
+			with: /\A[0-9]+\z/,
+			message: 'solo permite numeros'}
+	validates :celular,
+		:presence => {
+			message: 'Este campo es requerido'},
+		:length => {
+			:maximum => 20,
+			:too_long => 'debe tener como maximo %{count} caracteres'},
+		:format => {
+			with: /\A[0-9]+\z/,
+			message: 'solo permite numeros'}
+
+	validates :email,
+		:presence =>{
+				message: 'Este campo es requerido'},
+		:length =>{
+				:maximum => 50,
+				:too_long => 'Debe tener como maximo %{count} caracteres'}
 end
